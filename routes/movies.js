76c22032fe518ba7582d62c32ff34e233bbc3bb0
Mitansh Chaudhari <mitansh807@gmail.com>
2024-03-28 23:49:40 -0500

    Task Single Movie Page Completed.

--- routes/movies.js
+++ routes/movies.js
@@ -12,4 +12,18 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+// Route for displaying a single movie's details
+router.get('/:id', async (req, res, next) => {
+    try {
+      const movie = await Movie.findById(req.params.id); // Find the movie by ID
+      if (movie) {
+        res.render('movieDetails', { title: movie.title, movie });
+      } else {
+        res.redirect('/movies'); // Redirect to the movies list if the movie is not found
+      }
+    } catch (error) {
+      next(error);
+    }
+  });  
+  
 module.exports = router;
